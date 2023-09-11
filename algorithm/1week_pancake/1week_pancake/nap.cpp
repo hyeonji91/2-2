c71bde91d16956a31f80a51785250e17f12608e4
hyeonji91 <hjkim1770@gmail.com>
2023-09-12 01:41:21 +0900

    <fix>: week1 알고리즘 과제 fix

--- algorithm/1week_pancake/1week_pancake/nap.cpp
+++ algorithm/1week_pancake/1week_pancake/nap.cpp
@@ -21,26 +21,27 @@ void main() {
 	int maxNapTime = 0;//최대 낮잠 시간
 	int maxNapHour;//최대 낮잠 시간을 hour, minutes으로 분리
 	int maxNapMin;
-	int maxNapIndex;
+	int maxNapIndex = 0;
 
+	//스케줄 개수 입력받기
 	do {
 		printf("Enter your schedule number(<100) : \n");
 		scanf("%d", &scheduleNum);
 
-	} while (scheduleNum >= 100 || scheduleNum <= 0); //일정 개수가 조건에 맞을때만 다음으로 넘어가기
+	} while (scheduleNum >= 100 || scheduleNum <= 0); //스케줄 개수가 조건에 맞을때만 다음으로 넘어가기
 
-	for (int i = 0; i < scheduleNum; i++) {
+	for (int i = 0; i < scheduleNum; i++) {//스케줄 입력받기
 		do {
 			printf("schedule #%d (All times will be greater than or equal 10:00 and less than or equal to 18:00:\n", i);
-			scanf("%d:%d %d:%d %s", &startAtSplit[i][0], &startAtSplit[i][1], &endAtSplit[i][0], &endAtSplit[i][1], scheduleTitle[i]);
-
+			scanf("%d:%d %d:%d ", &startAtSplit[i][0], &startAtSplit[i][1], &endAtSplit[i][0], &endAtSplit[i][1]);
+			gets_s(scheduleTitle[i]);
 		//계산을 위해 분으로 포맷
 		startFormatTime[i] = startAtSplit[i][0] * 60 + startAtSplit[i][1];
 		endFormatTime[i] = endAtSplit[i][0] * 60 + endAtSplit[i][1];
 		} while (startFormatTime[i] < 600 || (endFormatTime[i] > 1800));//입력 시간이 10:00 ~ 18:00
-
 	}
-	for (int i = 1; i < scheduleNum; i++) {
+
+	for (int i = 1; i < scheduleNum; i++) {//최대 낮잠 시간&그때의 index 구하기
 		napTime = startFormatTime[i] - endFormatTime[i - 1];
 		if (maxNapTime < napTime)
 		{
@@ -52,9 +53,9 @@ void main() {
 	maxNapHour = maxNapTime / 60;
 	maxNapMin = maxNapTime % 60;
 
-	if(maxNapTime >= 60)
-		printf("the longesst nap starts at %02d:%02d and will last for %d hours and %d minutes", endAtSplit[maxNapIndex - 1][0], endAtSplit[maxNapIndex - 1][1], maxNapHour, maxNapMin);
+	if(maxNapTime >= 60)//maxNapTime이 1시간 넘어갈때
+		printf("the longest nap starts at %02d:%02d and will last for %d hours and %d minutes", endAtSplit[maxNapIndex - 1][0], endAtSplit[maxNapIndex - 1][1], maxNapHour, maxNapMin);
 	else
-		printf("the longesst nap starts at %02d:%02d and will last for %d minutes", endAtSplit[maxNapIndex - 1][0], endAtSplit[maxNapIndex - 1][1], maxNapMin);
+		printf("the longest nap starts at %02d:%02d and will last for %d minutes", endAtSplit[maxNapIndex - 1][0], endAtSplit[maxNapIndex - 1][1], maxNapMin);
 
 }
