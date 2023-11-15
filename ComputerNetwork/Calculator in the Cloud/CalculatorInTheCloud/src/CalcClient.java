4374d4a16fc4a496bef5074a0fc05965bf7faa24
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2023-11-15 14:27:08 +0900

    <feat> server에서 client로 메세지 보내기, client에서 error code 처리

--- ComputerNetwork/Calculator in the Cloud/CalculatorInTheCloud/src/CalcClient.java
+++ ComputerNetwork/Calculator in the Cloud/CalculatorInTheCloud/src/CalcClient.java
@@ -2,30 +2,76 @@ import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
+/*
+[PROTOCOL] Client to Server
+#1 CALCULATE
+[operator] [first number] [second number]
+- operator : add, sub, mul, div (not case-sensitive)
+#2 EXIT
+if you want exit program, enter q (not case-sensitive)
+ */
+
 public class CalcClient {
     public static void main(String[] arg){
-        BufferedReader in = null;
-        BufferedWriter out = null;
+        BufferedReader br = null;
+        String fileName = "serverInfo.dat";
+        Scanner in = null;
+        PrintWriter out = null;
         Socket socket = null;
         Scanner sc = null;
         String clientText;
         String serverText;
 
-        try{
-            socket = new Socket("127.0.0.1", 9999);
+        String IP = "";
+        int portNum = 0;
 
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        //IP address, port number 파일에서 입력받기
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            IP = br.readLine();
+            portNum = Integer.parseInt(br.readLine());
+            System.out.println(IP + " " + portNum);
+            br.close();
+        }catch (IOException e) {
+            System.out.println("Can not open file " +fileName + ". Set IP address and port number default");
+            IP = "127.0.0.1";
+            portNum = 9999;
+        }
+
+        try{
+            socket = new Socket(IP, portNum);
+
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
             sc = new Scanner(System.in);
 
+            System.out.println("===========[Calculator in the Cloud]==============================");
+            System.out.println("FORMAT : [operator] [first number] [second number]");
+            System.out.println("- operator : add, sub, mul, div (not case-sensitive)");
+            System.out.println("if you want exit program, enter 'q' (not case-sensitive)");
+            System.out.println("==================================================================");
             while(true) {
+                //client to server
                 System.out.print("계산식 >> ");
                 clientText = sc.nextLine();
-                out.write(clientText + "\n");
-                out.flush();
-                if(clientText.equalsIgnoreCase("bye"))
+                out.println(clientText);
+                if(clientText.equalsIgnoreCase("q"))
                     break;
-                System.out.println("계산결과 : " + in.readLine());
+
+                //from server
+                String success = in.nextLine(); //성공:s 실패:e
+                if(success.equals("s")){//Success
+                    System.out.print("[SUCCESS] ");
+                    String msg = in.nextLine();
+                    System.out.println("msg: "+msg);
+                }
+                else{//Error
+                    System.out.print("[ERROR]");
+                    String msg = in.nextLine();
+                    System.out.println("msg: "+msg);
+                    //Integer errorCode = Integer.parseInt(in.nextLine());
+                    printErrorMsg(Integer.parseInt(msg));
+                }
             }
         } catch(IOException e){
             System.out.println(e.getMessage());
@@ -37,6 +83,39 @@ public class CalcClient {
             }catch(IOException e) {
                 System.out.println("서버와의 채팅 주 오류가 발생했습니다.");
             }
+        }
+    }
+
+    public static void printErrorMsg(int errorCode){
+        /*
+        [PROTOCOL] server to client
+        #1 ERROR
+        [400] format error
+        [401] not number
+        [402] divided by ZERO
+        [403] not operator
+         */
+        switch(errorCode){
+            case 400:
+                System.out.println("Format error");
+                System.out.println("Please enter it in this format : [operator] [first number] [second number]");
+                break;
+            case 401:
+                System.out.println("Not number");
+                System.out.println("The second and third values must be number");
+                break;
+            case 402:
+                System.out.println("Divided by ZERO");
+                System.out.println("Cannot be divided by ZERO");
+                break;
+            case 403:
+                System.out.println("Not operator");
+                System.out.println("The first value is must be operator");
+                System.out.println("You can only use this operators : add, sub, mul, div (not case-sensitive)");
+                break;
+            default:
+                System.out.println("error");
+                break;
         }
     }
 }
--- ComputerNetwork/Calculator in the Cloud/CalculatorInTheCloud/src/CalcServer.java
+++ ComputerNetwork/Calculator in the Cloud/CalculatorInTheCloud/src/CalcServer.java
@@ -5,34 +5,71 @@ import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/*
+[PROTOCOL] server to client
+#1 ERROR
+[400] format error
+[401] not number
+[402] divided by ZERO
+[403] not operator
+ */
+
 public class CalcServer {
-    public static String calc(String exp){
-        StringTokenizer st = new StringTokenizer(exp, " ");
-        if(st.countTokens() != 3)
-            return "error";
-        String res = "";
-        int op1 = Integer.parseInt(st.nextToken());
-        String opcode = st.nextToken();
-        int op2 = Integer.parseInt(st.nextToken());
-        switch(opcode){
-            case "+":
-                res = Integer.toString(op1 + op2);
-                break;
-            case "-":
-                res = Integer.toString(op1-op2);
-                break;
-            case "*":
-                res = Integer.toString(op1*op2);
-                break;
-            case "/":
-                if(op2 == 0)
-                    res = "error";
-                else
-                    res = Double.toString((double) op1 /op2);
-            default:
-                res = "error";
+    public static class ReturnValue{
+        public String success = "s"; //s:success, e:error
+        public Integer errorCode;
+        public Double result;
+    }
+    public static ReturnValue calc(String exp){//계산하기
+        ReturnValue returnValue = new ReturnValue();
+        double result = 0;
+        StringTokenizer st = new StringTokenizer(exp, " ");//나누기
+        //3개가 아니면 포맷에러
+        if(st.countTokens() != 3){
+            returnValue.success = "e";
+            returnValue.errorCode = 400;
+            return returnValue;
         }
-        return res;
+
+        String opcode = st.nextToken().toLowerCase();
+        double op1 = 0, op2 = 0;
+        try{
+            op1 = Double.parseDouble(st.nextToken());
+            op2 = Double.parseDouble(st.nextToken());
+        }catch(NumberFormatException e){//double이 아니면 not number 에러
+            returnValue.success = "e";
+            returnValue.errorCode = 401;
+        }
+        //계산하기
+        switch(opcode){
+            case "add":
+                result = op1 + op2;
+                returnValue.result = result;
+                break;
+            case "sub":
+                result = op1-op2;
+                returnValue.result = result;
+                break;
+            case "mul":
+                result = op1*op2;
+                returnValue.result = result;
+                break;
+            case "div":
+                if(op2 == 0){//divided by zero 에러
+                    returnValue.success = "e";
+                    returnValue.errorCode = 402;
+                }
+                else{
+                    result = op1 /op2;
+                    returnValue.result = result;
+                }
+                break;
+            default://not operator 에러
+                returnValue.success = "e";
+                returnValue.errorCode = 403;
+        }
+        //System.out.println(returnValue.success + returnValue.errorCode + returnValue.result);
+        return returnValue;
     }
 
     public static void main (String[] arg) throws IOException{
@@ -41,8 +78,8 @@ public class CalcServer {
         System.out.println("연결을 기다리고 있습니다......");
         ExecutorService pool = Executors.newFixedThreadPool(20);
         while(true) {
-            Socket socket = listener.accept();
-            pool.execute(new CalcThread(socket));
+            Socket socket = listener.accept();//포트 열어두고 연결을 기다리다가 요청오면 소캣 생성
+            pool.execute(new CalcThread(socket));//스레드만들기
         }
     }
 
@@ -57,39 +94,57 @@ public class CalcServer {
         public void run(){
             System.out.println("Connected: "+ socket);
 
-            BufferedReader in = null;
-            BufferedWriter out = null;
+            Scanner in = null;
+            PrintWriter out = null;
             String ClientText;
             String ServerText;
 
             try{
-                System.out.println("연결되었습니다.");
-
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream());
 
                 while(true) {
-                    ClientText = in.readLine();
-                    if(ClientText.equalsIgnoreCase("bye")){
+                    //from client
+                    ClientText = in.nextLine();
+                    if(ClientText.equalsIgnoreCase("q")){
                         System.out.println("클라이언트에서 연결을 종료했습니다");
                         break;
                     }
+                    //계산하기
                     System.out.println(ClientText);
-
-                    out.write(calc(ClientText)+"\n");
-                    out.flush();
+                    ReturnValue returnValue = calc(ClientText);
+                    //server to client
+                    out.println(returnValue.success +"\n");//성공인지 에러인지 반환
+                    if(returnValue.success.equals("s")) {//성공이면 계산 값 반환
+                        out.println(returnValue.result.toString());
+                        out.flush();
+                        System.out.println("s: " +returnValue.result);
+                    }
+                    else {//실패면 에러코드 반환
+                        out.println(returnValue.errorCode.toString());
+                        out.flush();
+                        System.out.println("e: " + returnValue.errorCode);
+                    }
                 }
             } catch(IOException e){
                 System.out.println("Error : "+ socket);
             } finally{
                 try {
-
                     if(socket != null)
                         socket.close();
                 } catch(IOException e){
                     System.out.println("Closed : "+socket);
                 }
             }
+        }
+    }
+
+    public static void closeConnect(Socket socket){
+        try {
+            if(socket != null)
+                socket.close();
+        } catch(IOException e){
+            System.out.println("Closed : "+socket);
         }
     }
 }
